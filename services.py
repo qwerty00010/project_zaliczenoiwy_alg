302327bc422c517a5916cecfd14742db0f33c26d
barolzy <bartek.onieszczuk2@gmail.com>
2026-05-27 08:57:38 +0200

    moduł usług transportowych zadanie z czesci 2

---
+++ services.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from transport import Transport, bike
+
+class TransportServices(ABC):
+    def __init__(self):
+        self.available = True
+
+    @abstractmethod
+    def create_transport(self) -> Transport:
+        pass
+
+    @abstractmethod
+    def transport_name(self) -> str:
+        pass
+
+    def order_transport(self):
+        transport = self.create_transport()
+
+        if self.available:
+            print(f"Typ pojazdu: {transport.vehicle_type()}")
+            print(f"Przewidywany czas przyjazdu: {transport.arrival_time()}")
+            print(f"Przewidywany czas podróży: {transport.travel_time()}")
+        else:
+            print(f"Przepraszamy, transport {self.transport_name()} jest obecnie niedostępny.")
+
+class BikeService(TransportServices):
+    def create_transport(self) -> Transport:
+        return Bike()
+
+    def transport_name(self) -> str:
+        return "Bike"
+
+
+class ScooterService(TransportServices):
+    def create_transport(self) -> Transport:
+        from transport import Scooter
+        return Scooter()
+
+    def transport_name(self) -> str:
+        return "Scooter"
+
+
+class TaxiService(TransportServices):
+    def create_transport(self) -> Transport:
+        from transport import Taxi
+        return Taxi()
+
+    def transport_name(self) -> str:
+        return "Taxi"
