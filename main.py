06078d052d80d4301f28e4dca771b0893331efd9
qwerty00010 <mocnygazniepido@gmail.com>
2026-05-27 09:11:08 +0200

    First version of project v1

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from customer import Customer
 from services import ScooterService
 
-# 1. Tworzymy klienta
 klient = Customer("Jan", "Kowalski", "jan@op.pl", "500-600-700")
 
-# 2. POPRAWKA: Poprawna nazwa zmiennej dla serwisu hulajnóg
 serwis_scootera = ScooterService()
 
-# 3. POPRAWKA: Przekazujemy właściwą zmienną (bez zbędnego tekstowego "Scooter")
 zamowiony_pojazd = klient.order_transport(serwis_scootera)
